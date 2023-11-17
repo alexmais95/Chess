7e2e44c3ecf9db631f31505f18281b9288581372
alexmais <alexlove9596@gmail.com>
2023-11-17 15:15:17 +0200

    chenge dotenv-python to dotenv-django. chenge to manage.py and wsgi.py(add dotenv_read)

--- chess/chess/wsgi.py
+++ chess/chess/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+
 import os
 
+import dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chess.settings')
 
 application = get_wsgi_application()
+
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
